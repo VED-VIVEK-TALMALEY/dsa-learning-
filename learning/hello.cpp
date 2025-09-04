e4d00c5a7771cfceab26fb5ef53eb05b6a35eb6e
VED TALMALEY <vedwork21@gmail.com>
2025-09-04 15:45:04 +0530

    day 1

---
+++ learning/hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+    string name = "ved";
+    string *n= &name ;
+    cout<<name<<endl;
+    cout<<n<<endl;  
+    /*task 2 */
+    int age = 21;
+    int *page =&age;
+    cout<<*page;
+    cout<<page;
+    
+
+    
+}
